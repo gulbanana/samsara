ebe03d5446e02791bae3d7b6ed4997cebdac088d
Thomas Castiglione <castiglione@mac.com>
2022-01-23 22:54:04 +0800

    add some more ui elements

--- State/Game.cs
+++ State/Game.cs
@@ -4,8 +4,25 @@ public class Game
 {
     public int Width { get; set; }
     public int Height { get; set; }
+    public bool Alive { get; set; }
+    public TimeSpan? LifeSpan { get; set; }
 
     public void Tick(TimeSpan elapsed)
     {
+        if (Alive)
+        {
+            LifeSpan -= elapsed;
+
+            if (LifeSpan <= TimeSpan.Zero)
+            {
+                Alive = false;
+            }
+        }
+    }
+
+    public void DoLive()
+    {
+        Alive = true;
+        LifeSpan = TimeSpan.FromSeconds(10);
     }
 }
